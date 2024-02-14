a3e52c4c707a87016a9a813d28c714ce8a2f6541
RubeshR143681 <rubeshr81@gmail.com>
2024-02-14 17:41:02 +0530

    final

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -18,28 +18,36 @@ const Category = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div className="px-5 mt-3">
-      <div className="d-flex justify-content-center">
-        <h3>Cetegory List</h3>
-      </div>
-      <Link to="/dashboard/add_category" className="btn btn-success">
-        Add Cetegory
-      </Link>
-      <div className="mt-3">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {category.map((c) => (
+    <div
+      style={{
+        background: "linear-gradient(to right, #E5D9F2, #CDC1FF)",
+        height: "100vh",
+        padding: "20px",
+      }}
+    >
+      <div className="px-5 mt-3">
+        <div className="d-flex justify-content-center">
+          <h3>Cetegory List</h3>
+        </div>
+        <Link to="/dashboard/add_category" className="btn btn-success">
+          Add Cetegory
+        </Link>
+        <div className="mt-3">
+          <table className="table">
+            <thead>
               <tr>
-                <td>{c.name}</td>
+                <th>Name</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {category.map((c) => (
+                <tr>
+                  <td>{c.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -55,9 +55,21 @@ const Home = () => {
       });
   };
   return (
-    <div>
+    <div
+      style={{
+        background: "linear-gradient(to right, #E5D9F2, #CDC1FF)",
+        height: "100vh",
+        padding: "20px",
+      }}
+    >
       <div className="p-3 d-flex justify-content-around mt-3">
-        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+        <div
+          className="px-3 pt-2 pb-3 border  shadow-sm w-25"
+          style={{
+            borderRadius: "10px",
+            backgroundColor: "white",
+          }}
+        >
           <div className="text-center pb-1">
             <h4>Admin</h4>
           </div>
@@ -67,7 +79,10 @@ const Home = () => {
             <h5>{adminTotal}</h5>
           </div>
         </div>
-        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+        <div
+          className="px-3 pt-2 pb-3 border shadow-sm w-25"
+          style={{ borderRadius: "10px", backgroundColor: "white" }}
+        >
           <div className="text-center pb-1">
             <h4>Employee</h4>
           </div>
@@ -77,38 +92,43 @@ const Home = () => {
             <h5>{employeeTotal}</h5>
           </div>
         </div>
-        <div className="px-3 pt-2 pb-3 border shadow-sm w-25">
+        <div
+          className="px-3 pt-2 pb-3 border shadow-sm w-25"
+          style={{ borderRadius: "10px", backgroundColor: "white" }}
+        >
           <div className="text-center pb-1">
             <h4>Salary</h4>
           </div>
           <hr />
           <div className="d-flex justify-content-between">
             <h5>Total:</h5>
-            <h5>${salaryTotal}</h5>
+            <h5>₹ {salaryTotal}</h5>
           </div>
         </div>
       </div>
-      <div className="mt-4 px-5 pt-3">
+      <div className="mt-5 px-5 pt-3">
         <h3>List of Admins</h3>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {admins.map((a) => (
+        <div style={{ borderRadius: "10px", marginTop: "20px" }}>
+          <table className="table">
+            <thead>
               <tr>
-                <td>{a.email}</td>
-                <td>
-                  <button className="btn btn-info btn-sm me-2">Edit</button>
-                  <button className="btn btn-warning btn-sm">Delete</button>
-                </td>
+                <th>Email</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {admins.map((a) => (
+                <tr>
+                  <td>{a.email}</td>
+                  <td>
+                    <button className="btn btn-info btn-sm me-2">Edit</button>
+                    <button className="btn btn-warning btn-sm">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
